ecb3e49a7d0f2e7e12cae17fce28a7465d5baf14
Spiffical <spencer.bialek@gmail.com>
2025-09-22 15:13:06 -0700

    Add initial files: .gitignore, home page, and App Page 1

---
+++ home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home", page_icon=":house:")
+
+st.write("""
+# Home
+
+This is the home page of the app.
+""")
---
+++ pages/App_Page_1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="App Page 1", page_icon=":page_with_curl:")
+
+st.write("""
+# App Page 1
+
+This is the first page of the app.
+""")
