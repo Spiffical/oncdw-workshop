85da08ced397f8d1928712590d51209abaeddc06
Spiffical <spencer.bialek@gmail.com>
2025-10-20 15:49:04 -0700

    Add functionality to home page for location and archive file retrieval; update .gitignore and requirements

--- home.py
+++ home.py
@@ -1,9 +1,43 @@
 import streamlit as st
+import os
+from onc import ONC
+import pandas as pd
 
-st.set_page_config(page_title="Home", page_icon=":house:")
+st.title("Oceans 3.0 Open API Playground")
+st.metric("Web Services", 2, 1)
 
-st.write("""
-# Home
+token = os.getenv("ONC_TOKEN")
+onc = ONC(token)
 
-This is the home page of the app.
-""")
+st.header(":blue[Discovery Services]")
+
+st.subheader(":green[Return locations]")
+
+st.markdown(":blue-badge[GET] `/locations`")
+
+location_code = st.text_input("locationCode", placeholder="FGPD")
+
+if st.button("Run", key="location_button"):
+    param = {"locationCode": location_code}
+    location_info = onc.getLocations(param)
+    st.json(location_info)
+
+st.divider()
+
+st.header(":blue[Real-time Services]")
+
+st.subheader(":green[Return archivefiles]")
+
+st.markdown(":blue-badge[GET] `/archivefile`")
+
+device_code = st.text_input("deviceCode", value="BPR_BC")
+last_days = st.number_input("last days", value=4)
+
+if st.button("Run", key="archivefile_button"):
+    param = {
+        "deviceCode": device_code,
+        "dateFrom": f"-P{last_days}D",
+        "returnOptions": "all",
+    }
+    archivefile = onc.getArchivefile(param)
+    st.dataframe(pd.DataFrame(archivefile["files"]))
